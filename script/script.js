cdc5b81c04f25a6ff322c5f72a68af3d0dd34790
Francisco Franco <franciscofrancod@gmail.com>
2021-05-02 17:44:02 -0300

    Incluido functions do JS

---
+++ script/script.js
@@ -0,0 +1,11 @@
+function aplicando_cor(cor){
+    var elemento_1 = document.getElementById('colorindo').style.color = cor;
+}
+
+function ocultando_texto(){
+    var elemento_2 = document.getElementById('texto_1').style.display = 'none';
+}
+
+function mostrando_texto() {
+    var elemento_3 = document.getElementById('texto_1').style.display = 'block';
+}
